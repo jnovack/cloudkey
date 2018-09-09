46ffb7f85235a88549f86dc4e7bfab5fca8cc062
Justin J. Novack <jnovack@gmail.com>
2018-09-09 19:38:54 -0400

    custom network module

---
+++ src/network/network.go
@@ -0,0 +1,51 @@
+package network
+
+import (
+	"errors"
+	"net"
+
+	ipify "github.com/rdegges/go-ipify"
+)
+
+// LANIP gives you the first non-loopback IP address
+func LANIP() (string, error) {
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		return "", err
+	}
+	for _, iface := range ifaces {
+		if iface.Flags&net.FlagUp == 0 {
+			continue // interface down
+		}
+		if iface.Flags&net.FlagLoopback != 0 {
+			continue // loopback interface
+		}
+		addrs, err := iface.Addrs()
+		if err != nil {
+			return "", err
+		}
+		for _, addr := range addrs {
+			var ip net.IP
+			switch v := addr.(type) {
+			case *net.IPNet:
+				ip = v.IP
+			case *net.IPAddr:
+				ip = v.IP
+			}
+			if ip == nil || ip.IsLoopback() {
+				continue
+			}
+			ip = ip.To4()
+			if ip == nil {
+				continue // not an ipv4 address
+			}
+			return ip.String(), nil
+		}
+	}
+	return "", errors.New("network not found")
+}
+
+// WANIP gives you your WAN IP of the device
+func WANIP() (string, error) {
+	return ipify.GetIp()
+}
