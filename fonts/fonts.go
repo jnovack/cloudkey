27a54121f81a4c8d887e6698fbd78e052d0b2104
Justin J. Novack <jnovack@gmail.com>
2018-09-09 19:39:52 -0400

    update font loader

--- fonts/fonts.go
+++ fonts/fonts.go
@@ -10,18 +10,29 @@ import (
 	"github.com/golang/freetype/truetype"
 )
 
-// Font ...
-func Font() (*truetype.Font, error) {
-	reader := base64.NewDecoder(base64.StdEncoding, strings.NewReader(latoRegular))
+var assets map[string]string
+
+// Load ...
+func Load(name string) *truetype.Font {
+	reader := base64.NewDecoder(base64.StdEncoding, strings.NewReader(assets[name]))
 	file, err := ioutil.ReadAll(reader)
 	if err != nil {
 		log.Println(err)
-		return nil, err
+		return nil
 	}
 	font, err := freetype.ParseFont(file)
 	if err != nil {
 		log.Println(err)
-		return nil, err
+		return nil
 	}
-	return font, nil
+	return font
+}
+
+func init() {
+	assets = make(map[string]string)
+
+	// Define the permitted fonts
+	// Golang has no concept of dynamic variables, because it's a compliled language
+	// All variables must be declared, cannot iterate through files and load consts
+	assets["lato-regular"] = latoRegular
 }
