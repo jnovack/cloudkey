40473226058095a92cc2788e5d8f8a9a1e7c94b0
Justin J. Novack <jnovack@gmail.com>
2018-09-09 19:35:51 -0400

    upload inspiration folder

---
+++ inspiration/fbtest.go
@@ -0,0 +1,205 @@
+package fbtest
+
+// Copyright 2010 The Freetype-Go Authors. All rights reserved.
+// Use of this source code is governed by your choice of either the
+// FreeType License or the GNU General Public License version 2 (or
+// any later version), both of which can be found in the LICENSE file.
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"image"
+	"image/color"
+	"image/draw"
+	"io/ioutil"
+	"log"
+	"math"
+	"net"
+	"os"
+	"time"
+
+	"framebuffer"
+
+	"github.com/golang/freetype"
+	"golang.org/x/image/font"
+)
+
+var (
+	dpi      = flag.Float64("dpi", 72, "screen resolution in Dots Per Inch")
+	fontfile = flag.String("fontfile", "Lato2OFL/Lato-Regular.ttf", "filename of the ttf font")
+	hinting  = flag.String("hinting", "none", "none | full")
+	size     = flag.Float64("size", 12, "font size in points")
+	spacing  = flag.Float64("spacing", 1, "line spacing (e.g. 2 means double spaced)")
+	inverse  = flag.Bool("inverse", false, "black text on a white background")
+	clear    = flag.Bool("clear", false, "clear the screen")
+)
+
+var colors = []color.RGBA{
+	color.RGBA{0x00, 0x00, 0x00, 0xff},
+	color.RGBA{0x11, 0x11, 0x11, 0xff},
+	color.RGBA{0x22, 0x22, 0x22, 0xff},
+	color.RGBA{0x33, 0x33, 0x33, 0xff},
+	color.RGBA{0x44, 0x44, 0x44, 0xff},
+	color.RGBA{0x55, 0x55, 0x55, 0xff},
+	color.RGBA{0x66, 0x66, 0x66, 0xff},
+	color.RGBA{0x77, 0x77, 0x77, 0xff},
+	color.RGBA{0x88, 0x88, 0x88, 0xff},
+	color.RGBA{0x99, 0x99, 0x99, 0xff},
+	color.RGBA{0xaa, 0xaa, 0xaa, 0xff},
+	color.RGBA{0xbb, 0xbb, 0xbb, 0xff},
+	color.RGBA{0xcc, 0xcc, 0xcc, 0xff},
+	color.RGBA{0xdd, 0xdd, 0xdd, 0xff},
+	color.RGBA{0xee, 0xee, 0xee, 0xff},
+	color.RGBA{0xff, 0xff, 0xff, 0xff},
+}
+
+var text = []string{
+	"The quick brown fox =",
+	"jumps over the lazy dog.",
+	"(142) 867-5309 !#*@?+%",
+}
+
+func localIP() (string, error) {
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		return "", err
+	}
+	for _, iface := range ifaces {
+		if iface.Flags&net.FlagUp == 0 {
+			continue // interface down
+		}
+		if iface.Flags&net.FlagLoopback != 0 {
+			continue // loopback interface
+		}
+		addrs, err := iface.Addrs()
+		if err != nil {
+			return "", err
+		}
+		for _, addr := range addrs {
+			var ip net.IP
+			switch v := addr.(type) {
+			case *net.IPNet:
+				ip = v.IP
+			case *net.IPAddr:
+				ip = v.IP
+			}
+			if ip == nil || ip.IsLoopback() {
+				continue
+			}
+			ip = ip.To4()
+			if ip == nil {
+				continue // not an ipv4 address
+			}
+			return ip.String(), nil
+		}
+	}
+	return "", errors.New("network not found")
+}
+
+func drawEveryPixel() {
+	// Draw the guidelines.
+	// for i := 0; i < width; i++ {
+	// for j := 0; j < height; j++ {
+	// fb.Set(i, j, colors[x])
+	// fmt.Printf("%d: %dx%d\r", x, i, j)
+	// }
+	// time.Sleep(10 * time.Millisecond)
+	// }
+	// time.Sleep(1000 * time.Millisecond)
+}
+
+func fbtest() {
+	flag.Parse()
+
+	fb, err := framebuffer.Open("/dev/fb0")
+	if err != nil {
+		panic(err)
+	}
+
+	width := fb.Bounds().Max.X
+	height := fb.Bounds().Max.Y
+	fmt.Printf("Resolution: %dx%d pixels\r\n", width, height)
+
+	if *clear {
+		for x := range colors {
+			fmt.Printf("%d\r", x)
+			draw.Draw(fb, fb.Bounds(), image.NewUniform(colors[x]), image.ZP, draw.Src)
+			time.Sleep(32 * time.Millisecond)
+		}
+		draw.Draw(fb, fb.Bounds(), image.Black, image.ZP, draw.Src)
+		os.Exit(0)
+	}
+
+	hostname, _ := os.Hostname()
+	ipaddr, _ := localIP()
+	text = append(text, hostname)
+	text = append(text, ipaddr)
+
+	// Read the font data.
+	fontBytes, err := ioutil.ReadFile(*fontfile)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	f, err := freetype.ParseFont(fontBytes)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	// Initialize the context.
+	fg, bg := image.White, image.Black
+	// ruler := color.RGBA{0x22, 0x22, 0x22, 0xff}
+	if *inverse {
+		fg, bg = image.Black, image.White
+		// ruler = color.RGBA{0xdd, 0xdd, 0xdd, 0xff}
+	}
+
+	for x := 8; x < 15; x++ {
+		fmt.Printf("Font Size: %d\r\n", x)
+		// Clear screen
+		draw.Draw(fb, fb.Bounds(), bg, image.ZP, draw.Src)
+
+		// Draw the guidelines.
+		// for i := 0; i < width; i++ {
+		// 	fb.Set(0+i, 0, ruler)
+		// 	fb.Set(0+i, height-1, ruler)
+		// }
+		// for i := 0; i < height; i++ {
+		// 	fb.Set(0, 0+i, ruler)
+		// 	fb.Set(width-1, 0+i, ruler)
+		// }
+
+		// Setup new context
+		c := freetype.NewContext()
+		c.SetDPI(*dpi)
+		c.SetFont(f)
+		c.SetFontSize(float64(x)) // was (*size)
+		c.SetClip(fb.Bounds())
+		c.SetDst(fb)
+		c.SetSrc(fg)
+		switch *hinting {
+		default:
+			c.SetHinting(font.HintingNone)
+		case "none":
+			c.SetHinting(font.HintingNone)
+		case "full":
+			c.SetHinting(font.HintingFull)
+		}
+
+		// Draw the text.
+		pt := freetype.Pt(0, 0+int(c.PointToFixed(math.Round(float64(x)+1))>>6))
+		// pt := freetype.Pt(10, 10+int(c.PointToFixed(*size)>>6))
+		for _, s := range text {
+			_, err = c.DrawString(s, pt)
+			if err != nil {
+				log.Println(err)
+				return
+			}
+			pt.Y += c.PointToFixed(math.RoundToEven(float64(x)**spacing + 1))
+			// pt.Y += c.PointToFixed(*size * *spacing)
+		}
+		time.Sleep(3000 * time.Millisecond)
+	}
+}
---
+++ inspiration/inspiration.go
@@ -0,0 +1,105 @@
+// Copyright 2010 The Freetype-Go Authors. All rights reserved.
+// Use of this source code is governed by your choice of either the
+// FreeType License or the GNU General Public License version 2 (or
+// any later version), both of which can be found in the LICENSE file.
+
+// +build example
+//
+// This build tag means that "go install github.com/golang/freetype/..."
+// doesn't install this example program. Use "go run main.go" to run it or "go
+// install -tags=example" to install it.
+
+package main
+
+import (
+	"flag"
+	"image"
+	"image/color"
+	"image/draw"
+	"io/ioutil"
+	"log"
+
+	"framebuffer"
+
+	"github.com/golang/freetype"
+	"golang.org/x/image/font"
+)
+
+var (
+	dpi      = flag.Float64("dpi", 72, "screen resolution in Dots Per Inch")
+	fontfile = flag.String("fontfile", "Lato2OFL/Lato-Regular.ttf", "filename of the ttf font")
+	hinting  = flag.String("hinting", "none", "none | full")
+	size     = flag.Float64("size", 12, "font size in points")
+	spacing  = flag.Float64("spacing", 1.5, "line spacing (e.g. 2 means double spaced)")
+	wonb     = flag.Bool("whiteonblack", false, "white text on a black background")
+)
+
+var text = []string{
+	"’Twas brillig, and the slithy toves",
+	"Did gyre and gimble in the wabe;",
+	"All mimsy were the borogoves,",
+	"And the mome raths outgrabe.",
+}
+
+func main() {
+	flag.Parse()
+
+	// Read the font data.
+	fontBytes, err := ioutil.ReadFile(*fontfile)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	f, err := freetype.ParseFont(fontBytes)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	// Initialize the context.
+	fg, bg := image.Black, image.White
+	ruler := color.RGBA{0xdd, 0xdd, 0xdd, 0xff}
+	if *wonb {
+		fg, bg = image.White, image.Black
+		ruler = color.RGBA{0x22, 0x22, 0x22, 0xff}
+	}
+
+	fb, err := framebuffer.Open("/dev/fb0")
+	if err != nil {
+		panic(err)
+	}
+
+	// rgba := image.NewRGBA(image.Rect(0, 0, 640, 480))
+	draw.Draw(fb, fb.Bounds(), bg, image.ZP, draw.Src)
+	c := freetype.NewContext()
+	c.SetDPI(*dpi)
+	c.SetFont(f)
+	c.SetFontSize(*size)
+	c.SetClip(fb.Bounds())
+	c.SetDst(fb)
+	c.SetSrc(fg)
+	switch *hinting {
+	default:
+		c.SetHinting(font.HintingNone)
+	case "full":
+		c.SetHinting(font.HintingFull)
+	}
+
+	// Draw the guidelines.
+	for i := 0; i < 200; i++ {
+		fb.Set(10, 10+i, ruler)
+		fb.Set(10+i, 10, ruler)
+	}
+
+	// Draw the text.
+	pt := freetype.Pt(10, 10+int(c.PointToFixed(*size)>>6))
+	for _, s := range text {
+		_, err = c.DrawString(s, pt)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		pt.Y += c.PointToFixed(*size * *spacing)
+	}
+
+}
